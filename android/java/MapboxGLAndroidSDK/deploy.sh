8ee59a08b30d8475831f54ffab1d5fcfedca6501
Brad Leege <bleege@gmail.com>
2015-02-12 10:53:56 -0800

    #826 - Adding Travis encrypted variables for Sonatype Username and Password to deploy script

--- android/java/MapboxGLAndroidSDK/deploy.sh
+++ android/java/MapboxGLAndroidSDK/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-../gradlew -b build.gradle clean assembleRelease uploadArchives
+../gradlew -b build.gradle clean assembleRelease uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
