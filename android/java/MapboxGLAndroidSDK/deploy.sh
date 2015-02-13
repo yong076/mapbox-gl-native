5be1623341dde68cdf3d8edab4f9168ff1d7e433
Brad Leege <bleege@gmail.com>
2015-02-13 11:32:53 -0800

    #826 - Updating Gradle command to pull encrypted Travis Key and Password variables for GPG key that's stored on S3

--- android/java/MapboxGLAndroidSDK/deploy.sh
+++ android/java/MapboxGLAndroidSDK/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-../gradlew -b build.gradle clean assembleRelease uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+../gradlew -b build.gradle clean assembleRelease uploadArchives -PnexusUsername=$SONATYPE_USERNAME -PnexusPassword=$SONATYPE_PASSWORD -Psigning.keyId=$SIGNING_KEYID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=s3://mapbox/mapbox-gl-native/android/keys/mapbox-android-key.gpg
