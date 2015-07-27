7a707968398a223784486c7056dbc8f3f4d63175
Bruno de Oliveira Abinader <bruno@mapbox.com>
2015-07-27 19:39:31 +0300

    Install prebuilt Qt 4.7 dependency for OSX

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -8,3 +8,21 @@ git submodule update --init .mason
 
 mapbox_time "install_xcpretty" \
 gem install xcpretty --no-rdoc --no-ri --no-document --quiet
+
+if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] ; then
+    # Install and add awscli to PATH for copying Qt 4.7 prebuilt binaries
+    mapbox_time "install_awscli" \
+    brew install awscli
+
+    S3_PREFIX=s3://mason-binaries/prebuilt
+    QT_TARBALL=qt-4.7-osx-yosemite.tgz
+
+    mapbox_time "download_prebuilt_qt47" \
+    aws s3 cp ${S3_PREFIX}/${QT_TARBALL} .
+
+    mapbox_time "ensure_opt_directory" \
+    sudo mkdir -p /opt
+
+    mapbox_time "unpack_prebuilt_qt47" \
+    sudo tar xzf ${QT_TARBALL} -C /opt
+fi
--- scripts/osx/setup.sh
+++ scripts/osx/setup.sh
@@ -4,5 +4,9 @@
 # Ensure mason is on the PATH
 export PATH="`pwd`/.mason:${PATH}" MASON_DIR="`pwd`/.mason"
 
+# Export Qt 4.7 paths
+export PATH="/opt/qt-4.7/bin:${PATH}"
+export PKG_CONFIG_PATH="/opt/qt-4.7/lib/pkgconfig"
+
 # Set the core file limit to unlimited so a core file is generated upon crash
 ulimit -c unlimited -S
